5697aa5e31cca400941dd4e83ea14c0b65839413
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2024-01-06 12:54:11 -0700

    java(17): Add libstdcxx5 for Spark in Minecraft servers
    Spark relys on libstdc++ for it's async profiler. This should fix the error Unable to initialise the async-profiler engine because libstdc++ is not installed.

--- java/17/image.nix
+++ java/17/image.nix
@@ -12,5 +12,5 @@ buildImage {
     echo ":/home/container$ $MODIFIED_STARTUP"
     $MODIFIED_STARTUP
   '';
-  contents = [ pkgs.openjdk17-bootstrap ];
+  contents = [ pkgs.openjdk17-bootstrap pkgs.libstdcxx5 ];
 }
