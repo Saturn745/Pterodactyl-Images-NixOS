d75c7a7c8758375c1d3f5bc5bd64fe5c77b2bddd
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2024-01-06 10:50:50 -0700

    So clsoe

--- java/17/image.nix
+++ java/17/image.nix
@@ -8,9 +8,9 @@ buildImage {
     cd /home/container
     java -version
     echo "Starting up..."
-    MODIFIED_STARTUP=`eval echo $(echo $\{STARTUP} | sed -e 's/{{/$\{/g' -e 's/}}/}/g')`
-    echo ":/home/container$ $\{MODIFIED_STARTUP}"
-    $\{MODIFIED_STARTUP}
+    MODIFIED_STARTUP=`eval echo $(echo $STARTUP | ${pkgs.gnused}/bin/sed -e 's/{{/$\{/g' -e 's/}}/}/g')`
+    echo ":/home/container$ $MODIFIED_STARTUP"
+    $MODIFIED_STARTUP
   '';
   contents = [ pkgs.jre17_minimal ];
 }
